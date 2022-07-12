6ba9adf28f8332fbc64faee024ca765b91ec3571
Vikincer <vikinc@163.com>
2022-07-12 20:20:58 +0800

    增加首页index

--- src/main/java/org/vikinc/community/controller/HelloController.java
+++ src/main/java/org/vikinc/community/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
